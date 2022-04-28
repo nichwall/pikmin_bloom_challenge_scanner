1978c5e9d91efb797d02911e8fbedc47f7e766d0
Nicholas Wallace <nicholaslwallace@gmail.com>
2022-04-28 16:53:44 -0600

    Added color scanning and first pass at heart detection

--- src/pikmin_image_parser.py
+++ src/pikmin_image_parser.py
@@ -14,9 +14,9 @@ from scipy import stats
 # Load templates and return in an array
 def load_heart_templates():
     # Load templates
-    template_full  = imread("template_full_heart.png")
-    template_half  = imread("template_half_heart.png")
-    template_empty = imread("template_empty_heart.png")
+    template_full  = imread("../templates/template_full_heart.png")
+    template_half  = imread("../templates/template_half_heart.png")
+    template_empty = imread("../templates/template_empty_heart.png")
 
     # Convert to grayscale
     template_full_gray = rgb2gray(template_full)
@@ -82,7 +82,7 @@ def partition_image(image_path, heart_y_coord):
 
     # Partition bottom
     # down ~20
-    partition_bot = heart_y_coord + 20
+    partition_bot = heart_y_coord + 30
 
     # Get image shape
     # Load image
@@ -103,8 +103,92 @@ def partition_image(image_path, heart_y_coord):
 # Determine what color the pikmin is
 # based on the name
 def get_pikmin_color_by_name(pikmin_image):
-    # Load color templates
+    # Load color templates and convert to grayscale
+    color_templates = {}
+    for key in ["red", "yellow", "blue", "purple", "white", "winged"]:
+        color_templates[key] = rgb2gray(imread("../templates/color_"+key+".png"))
 
-heart_y_coord = get_heart_locations("full_hearts.jpg")
-partition_image("full_hearts.jpg", heart_y_coord)
+    # Convert to grayscale
+    image_gray = rgb2gray(pikmin_image)
+
+    # Determine which has a match
+    for key, val in color_templates.items():
+        result = match_template(image_gray, val)
+        if len( peak_local_max(result, threshold_abs=0.9, exclude_border=20) ) > 0:
+            return key
+
+    # If no match, ask user for input
+    plt.ion()
+    plt.figure()
+    plt.imshow(pikmin_image)
+    plt.show()
+
+    color = input("What color is this pikmin?")
+    plt.close()
+
+    return color
+
+
+# Determine how many friendship hearts the Pikmin
+# has. This is done by determining where the left side of the leftmost heart is
+# and currently only returns how many heart icons there are
+def get_pikmin_heart_icon_count(pikmin_image):
+    image_gray = rgb2gray(pikmin_image)
+
+    # Find templates in image
+    results = []
+    heart_templates = load_heart_templates()
+    for template in heart_templates:
+        result_full  = match_template(image_gray, template)
+        results.append(result_full)
+
+
+    # Get heart locations
+    heart_locations = []
+    for template_result in results:
+        template_h, template_w = heart_templates[0].shape
+        for x,y in peak_local_max(template_result, threshold_abs=0.9, exclude_border=10):
+            heart_locations.append([x,y])
+
+
+    # Get X coordinate of top of hearts
+    try:
+        heart_x_coord = np.unique( np.transpose(heart_locations)[1] )
+        heart_x_coord.sort()
+        left_heart = heart_x_coord[0]
+        #print(f"Heart locations: {heart_x_coord}")
+
+        # Convert to number of hearts
+        # TODO this does not detect whether the last heart is empty
+        #      or whether the final heart is full (4 hearts)
+        if left_heart < 40:
+            return 4
+        elif left_heart < 57:
+            return 3
+        elif left_heart < 67:
+            return 2
+        else:
+            return 1
+    except:
+        plt.ion()
+        plt.figure()
+        plt.imshow(pikmin_image)
+        plt.show()
+        heart_count = input("How many hearts? (-1 for nothing)")
+        plt.close()
+
+        return heart_count
+
+
+#path_to_image = "../screenshots/full_hearts.jpg"
+path_to_image = "../screenshots/small_hearts.jpg"
+
+heart_y_coord = get_heart_locations(path_to_image)
+pikmin_images = partition_image(path_to_image, heart_y_coord)
+
+for i in range(len(pikmin_images)):
+    pikmin_hearts = get_pikmin_heart_icon_count( pikmin_images[i] )
+    color = get_pikmin_color_by_name( pikmin_images[i] )
+    print(f"Pikmin {i} is {color} with {pikmin_hearts} heart icons")
+
 
